85e33d0e8682ca0c642ba2d42d82bd87ccd6f546
'Alexandre <alexandregomes@outlook.com>
2017-02-20 19:15:38 -0300

    Criacao da tela que mostra as transações.

--- HireMe/HireMe/VisualizarTransacao.xaml.cs
+++ HireMe/HireMe/VisualizarTransacao.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace HireMe
+namespace RockClient
 {
     /// <summary>
     /// Interaction logic for VisualizarTransacao.xaml
@@ -21,7 +21,29 @@ namespace HireMe
     {
         public VisualizarTransacao()
         {
+            
+
             InitializeComponent();
+
+            
+
+        }
+
+        private void dataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+            List<Transaction> transacoes = new List<Transaction>();
+            Transaction t1 = new Transaction(1000, "Tipo1", new Card(), 1);
+            Transaction t2 = new Transaction(1020, "Tipo2", new Card(), 2);
+            Transaction t3 = new Transaction(1030, "Tipo3", new Card(), 3);
+            Transaction t4 = new Transaction(1040, "Tipo4", new Card(), 5);
+            
+            /*
+            var grid = sender as DataGrid;
+            grid.ItemsSource = transacoes;
+            */
+
+            this.dataGrid.ItemsSource = transacoes;
         }
     }
 }
