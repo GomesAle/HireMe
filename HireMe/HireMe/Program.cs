6fceb72f573e465ed145a0b8e788a8a4523d6ecb
'Alexandre <alexandregomes@outlook.com>
2017-02-20 15:44:55 -0300

    Criei a classe que envia as informações para o Servidor.

---
+++ HireMe/HireMe/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockClient
+{
+    class Program
+    {
+        const int PORT_NO = 5000;
+        const string SERVER_IP = "127.0.0.1";
+
+        public String enviar(String mensagem)
+        {
+            //---create a TCPClient object at the IP and port no.---
+            TcpClient client = new TcpClient(SERVER_IP, PORT_NO);
+
+            NetworkStream nwStream = client.GetStream();
+                string textToSend = mensagem;
+                byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(textToSend);
+
+                //---send the text---
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+
+                //---read back the text---
+                byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+                int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+
+            client.Close();
+
+            //Retorna a resposta do servidor
+            return Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+
+           
+            
+        }
+    }
+}
