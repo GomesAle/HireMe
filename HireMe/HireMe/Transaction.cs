0b27e430bef3ee66b9050dc3d8c4d2f4844ba2b5
'Alexandre <alexandregomes@outlook.com>
2017-02-20 15:46:13 -0300

    Alterei o tipo de um atributo e o namespace.

--- HireMe/HireMe/Transaction.cs
+++ HireMe/HireMe/Transaction.cs
@@ -4,16 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HireMe
+namespace RockClient
 {
     class Transaction
     {
-        Decimal amount; //Valor da transação
+        float amount; //Valor da transação
         String type; //Tipo da transação
         Card card; //Propriedades do cartão
         int number; //Número de parcelas, se for uma transação de crédito parcelado
 
-        public Transaction(Decimal amount, String type, 
+        public Transaction(float amount, String type, 
             Card card, int number)
         {
             this.amount = amount;
