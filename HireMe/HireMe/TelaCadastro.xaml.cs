cfaab403854bb1d712e4056a35c1a1b02b4656c1
'Alexandre <alexandregomes@outlook.com>
2017-02-20 15:43:32 -0300

    Implementei a criação de conta.

--- HireMe/HireMe/TelaCadastro.xaml.cs
+++ HireMe/HireMe/TelaCadastro.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-namespace HireMe
+namespace RockClient
 {
     /// <summary>
     /// Interaction logic for TelaCadastro.xaml
     /// </summary>
     public partial class TelaCadastro : Window
     {
+        Controller controller;
+
         public TelaCadastro()
         {
             InitializeComponent();
+            controller = new Controller();
         }
+
+        private void buttonCadastrar_Click(object sender, RoutedEventArgs e)
+        {
+            long numero = DateTime.Now.Ticks;//Tick é um long de até 19 digitos
+
+            DateTime dataExpiracao = DateTime.Now.AddYears(5);//Põe cinco anos de validade do cartão.
+
+            Card cartao = new Card(this.textBoxNome.Text,
+                                    numero,
+                                    dataExpiracao,
+                                    this.comboBoxBandeira.Text,
+                                    Int32.Parse(this.textBoxSenha.Text),
+                                    this.comboBoxTipo.Text,
+                                    float.Parse(this.textBoxLimite.Text));
+
+            //Enviar o cartão criado
+            String cartaoJson  = JsonUtil.converterObjetoParaJson<Card>(cartao);
+            String resposta = controller.enviarMensagem(cartaoJson);
+
+            this.labelNumero.Content = "Cartão criado. O número dele é: "+numero;
+        }
+
+        
     }
 }
