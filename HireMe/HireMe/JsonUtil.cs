3597afddea6a54b6e2c06b9aa229810d6002cb8a
'Alexandre <alexandregomes@outlook.com>
2017-02-20 15:42:13 -0300

    Criei a classe para trabalhar com a conversão de json.

---
+++ HireMe/HireMe/JsonUtil.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RockClient
+{
+    class JsonUtil
+    {
+        public static string converterObjetoParaJson<T>(T obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            serializer.WriteObject(stream, obj);
+            stream.Position = 0;
+            StreamReader sr = new StreamReader(stream);
+
+            return sr.ReadToEnd();
+        }
+
+        public static Object converterJsonParaObjeto<T>(String json)
+        {
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(json));
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(T));
+            T obj = (T)serializer.ReadObject(stream);
+            return obj;
+        }
+    }
+}
