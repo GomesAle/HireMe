f5ed1695bf90df780fd6157e5e0f0db35c83cfba
'Alexandre <alexandregomes@outlook.com>
2017-02-20 19:16:36 -0300

    Adicionar os constrados para serialização  e os getters setters.

--- HireMe/HireMe/Transaction.cs
+++ HireMe/HireMe/Transaction.cs
@@ -1,25 +1,86 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RockClient
 {
+    [DataContract]
     class Transaction
     {
-        float amount; //Valor da transação
-        String type; //Tipo da transação
-        Card card; //Propriedades do cartão
+        [DataMember]
+        float amount;  //Valor da transação
+
+        [DataMember]
+        String type;  //Tipo da transação
+
+        [DataMember]
+        Card card;  //Propriedades do cartão
+
+        [DataMember]
         int number; //Número de parcelas, se for uma transação de crédito parcelado
+
+        public float Amount
+        {
+            get
+            {
+                return amount;
+            }
+
+            set
+            {
+                amount = value;
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+
+            set
+            {
+                type = value;
+            }
+        }
+
+        internal Card Card
+        {
+            get
+            {
+                return card;
+            }
+
+            set
+            {
+                card = value;
+            }
+        }
+
+        public int Number
+        {
+            get
+            {
+                return number;
+            }
+
+            set
+            {
+                number = value;
+            }
+        }
 
         public Transaction(float amount, String type, 
             Card card, int number)
         {
-            this.amount = amount;
-            this.type = type;
-            this.card = card;
-            this.number = number;
+            this.Amount = amount;
+            this.Type = type;
+            this.Card = card;
+            this.Number = number;
         }
     }
 }
