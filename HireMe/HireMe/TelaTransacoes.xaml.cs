8a2330f26140a323f29ae409a7dafcd206e7ffbb
'Alexandre <alexandregomes@outlook.com>
2017-02-20 19:15:06 -0300

    Criei a ligação dessa tela com a tela de mostrar transacoes.

---
+++ HireMe/HireMe/TelaTransacoes.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RockClient
+{
+    /// <summary>
+    /// Interaction logic for TelaTransacoes.xaml
+    /// </summary>
+    public partial class TelaTransacoes : Window
+    {
+        public TelaTransacoes()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCriarTransacao_Click(object sender, RoutedEventArgs e)
+        {
+            CriarTransacao criarTransacao = new CriarTransacao();
+            criarTransacao.Show();
+        }
+
+        private void buttonVisualizarTransacoes_Click(object sender, RoutedEventArgs e)
+        {
+            VisualizarTransacao visualizarTransacoes = new VisualizarTransacao();
+            visualizarTransacoes.Show();
+        }
+    }
+}
