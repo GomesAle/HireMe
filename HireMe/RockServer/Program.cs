e86b2030cbea23de1c8f33075a669db04acab295
GomesAle <alexandregomes@outlook.com>
2017-02-20 23:00:28 -0300

    Correção de nomes, comentários e implementação de lógica de criação de cartão.

--- HireMe/RockServer/Program.cs
+++ HireMe/RockServer/Program.cs
@@ -5,6 +5,9 @@ using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace RockServer
 {
@@ -12,88 +15,143 @@ namespace RockServer
     {
         static void Main(string[] args)
         {
-            const int PORT_NO = 5000;
-            const string SERVER_IP = "127.0.0.1";
-
-            
+            const int porta = 5000;
+            const string IP = "127.0.0.1";
 
             while (true)
             {
-                //---listen at the specified IP and port no.---
-                IPAddress localAdd = IPAddress.Parse(SERVER_IP);
-                TcpListener listener = new TcpListener(localAdd, PORT_NO);
-
+                //Começa a escutar o client
+                TcpListener listener = new TcpListener(IPAddress.Parse(IP), porta);
                 listener.Start();
 
-                //---incoming client connected---
+                //Se liga no client.
                 TcpClient client = listener.AcceptTcpClient();
-                Console.WriteLine("Ouvindo");
-                //---get the incoming data through a network stream---
-                NetworkStream nwStream = client.GetStream();
+
+               //Monta o array de bytes de acordo com o que vai vir do client.
+                NetworkStream networkStream = client.GetStream();
                 byte[] buffer = new byte[client.ReceiveBufferSize];
 
-                //---read incoming stream---
-                int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize);
+                //Lê os dados do Stream
+                int bytesRead = networkStream.Read(buffer, 0, client.ReceiveBufferSize);
 
-                //---convert the data received into a string---
-                string dataReceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                //Converte para bytes para String
+                string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead);
 
                 //Pegar o json recebido, transformar em objeto e fazer as checagens necessárias.
                 Card cartao = null;
                 Transaction transacao = null;
-                string dataSent = "xxx";
+                string dataSent = "";
 
                 if (dataReceived.Contains("cardholderName"))
                 {
-                    Console.WriteLine("Contem");
                     cartao = (Card)JsonUtil.converterJsonParaObjeto<Card>(dataReceived);
-                    Console.WriteLine(dataReceived);
                     if (cartao.cardholderName == "")
                     {
-                        Console.WriteLine("cardholder é vazio");
                         //Se estiver vazio, é porque está executando uma transacao
                         //Checa se está no banco.
                         //Se estiver, permite a transacao.
+                        DataTable tabela = executarSQL("select * from Card where number = " + cartao.number);
+                        Console.WriteLine("select * from Card where number = " + cartao.number);                                                        
+                        if (tabela.Rows.Count > 0)
+                        {
+                            //Cartão existe
+                            dataSent = "Cartão existe";
+                            Card c = new Card();
+                            c.cardBrand = tabela.Rows[0]["cardBrand"].ToString();
+                            c.cardholderName = tabela.Rows[0]["cardholderName"].ToString();
+                            c.expirationDate = new DateTime(int.Parse(tabela.Rows[0]["expirationDate"].ToString().Split('/')[2]), int.Parse(tabela.Rows[0]["expirationDate"].ToString().Split('/')[1]), int.Parse(tabela.Rows[0]["expirationDate"].ToString().Split('/')[0]));
+                            if (tabela.Rows[0]["hasPassword"].ToString() == "0")
+                            {
+                                c.hasPassword = false;
+                            }else
+                            {
+                                c.hasPassword = true;
+                            }
 
-                        //Se não estiver retorna erro. Cartão não encontrado.
-                        //Se a senha estiver errada. Retorna erro de senha.
+                            c.limit = float.Parse(tabela.Rows[0]["limit"].ToString());
+                            c.number = Int32.Parse(tabela.Rows[0]["number"].ToString());
+                            c.password = int.Parse(tabela.Rows[0]["password"].ToString());
+                            c.type = tabela.Rows[0]["type"].ToString();
+
+                            dataSent = JsonUtil.converterObjetoParaJson(c);
+                            Console.WriteLine(dataSent);
+
+                        }
+                        else{
+                            //Cartão não existe
+                            dataSent = "Cartão não existe";
+                        }
+
                     }
                     else
                     {
-                        dataSent = "volta";
                         //Se não estiver vazio, é porque está criando o cartão.
                         //Salva o cartão no banco
+
+                        //Converte de boolean para bit
+                        byte temSenha = 0;
+                        if (cartao.hasPassword)
+                        {
+                            temSenha = 1;
+                        }
+
+                        executarSQL("insert into Card values ('"+cartao.cardholderName+"', "+cartao.number+", '"+cartao.expirationDate.ToString("MM/dd/yyyy")+"', '"+cartao.cardBrand+"', "+cartao.password+", '"+cartao.type+"', "+temSenha+", "+cartao.limit+")");  
                     }
                 }
                 else
                 {
                     if (dataReceived.Contains("amount"))
                     {
-                        Console.WriteLine("Dado enviado que entra no transaction: " + dataReceived);
                         transacao = (Transaction)JsonUtil.converterJsonParaObjeto<Transaction>(dataReceived);
                         Console.WriteLine(dataReceived);
                         if (transacao.amount <= cartao.limit)
                         {
+                            dataSent = "Aprovado";
                             //Transacao efetuada com sucesso.
                             //Salvar no banco de dados
                             //Reenviar a mensagem de sucesso
+
+                            executarSQL("insert into Transacao values("+transacao.amount+", '"+transacao.type+"', "+cartao.number+", "+transacao.number+")");
                         }
                         else
                         {
-                            //Valor fora do limite
+                            dataSent = "Transação negada";
                         }
                     }
                 }    
                     
-
+                //Manda de volta para o client a string dataSent
                 byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(dataSent);
-                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+                networkStream.Write(bytesToSend, 0, bytesToSend.Length);
 
+                //Fechar para não ficar recebendo string vazias.
                 client.Close();
                 listener.Stop();
             }
+        }
 
+        //https://msdn.microsoft.com/pt-br/library/fksx3b4f.aspx
+        public static DataTable executarSQL(string sql)
+        {
+            SqlConnection sqlConnection = new SqlConnection("Data Source=ALEGOMES-PC;Initial Catalog=HireMe;Integrated Security=True");
+            SqlCommand cmd = new SqlCommand();
+            SqlDataReader reader;
 
+            cmd.CommandText = sql;
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection;
+
+            sqlConnection.Open();
+
+            reader = cmd.ExecuteReader();
+
+            sqlConnection.Close();
+
+            DataTable tabela = new DataTable();
+
+            tabela.Load(reader);
+
+            return tabela;
         }
     }
 }
